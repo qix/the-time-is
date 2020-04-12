7a055895f6b8ccbc32347ccecf96c64a3fef8543
Josh Yudaken <j@yud.co.za>
2020-04-12 10:49:31 -0400

    Format with black

--- main.py
+++ main.py
@@ -15,44 +15,70 @@ try:
     if len(sys.argv) == 2:
         random.seed(int(sys.argv[1]))
 except ValueError:
-    print('Usage: the-time-is [integer seed]', file=sys.stderr)
+    print("Usage: the-time-is [integer seed]", file=sys.stderr)
     sys.exit(1)
 
 digits = [
-    'one', 'two', 'three', 'four' , 'five', 'six', 'seven', 'eight', 'nine',
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
 ]
 
 past = [
-    *digits,  'quarter',
+    *digits,
+    "quarter",
 ]
 from_ten = [
-    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen',
-    'sixteen', 'seventeen', 'eighteen', 'nineteen',
-    'twenty', *[f'twenty {digit}' for digit in digits],
-    'thirty', *[f'thirty {digit}' for digit in digits],
-    'forty', *[f'forty {digit}' for digit in digits],
-    'fifty', *[f'fifty {digit}' for digit in digits],
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+    "twenty",
+    *[f"twenty {digit}" for digit in digits],
+    "thirty",
+    *[f"thirty {digit}" for digit in digits],
+    "forty",
+    *[f"forty {digit}" for digit in digits],
+    "fifty",
+    *[f"fifty {digit}" for digit in digits],
 ]
 
-hours = ['twelve', *digits, 'ten', 'eleven']
+hours = ["twelve", *digits, "ten", "eleven"]
 
-starts = ['the time is', 'it is']
+starts = ["the time is", "it is"]
 
 times = {}
-for ampm in ['am', 'pm']:
-    for hour, hour_name in enumerate(hours, (0 if ampm == 'am' else 12)):
-        times[(hour, 0)] = f'{random.choice(starts)} {hour_name} {ampm}'
+for ampm in ["am", "pm"]:
+    for hour, hour_name in enumerate(hours, (0 if ampm == "am" else 12)):
+        times[(hour, 0)] = f"{random.choice(starts)} {hour_name} {ampm}"
         for minute, digit in enumerate(digits, 1):
-            times[(hour, minute)] = f'{random.choice(starts)} {digit} past {hour_name} {ampm}'
+            times[
+                (hour, minute)
+            ] = f"{random.choice(starts)} {digit} past {hour_name} {ampm}"
         for minute, minute_name in enumerate(from_ten, 10):
-            times[(hour, minute)] = f'{random.choice(starts)} {hour_name} {minute_name} {ampm}'
-        times[(hour, 15)] = f'{random.choice(starts)} quarter past {hour_name} {ampm}'
+            times[
+                (hour, minute)
+            ] = f"{random.choice(starts)} {hour_name} {minute_name} {ampm}"
+        times[(hour, 15)] = f"{random.choice(starts)} quarter past {hour_name} {ampm}"
 
 times[(4, 20)] = times[(16, 20)] = "it is four twenty blaze it"
 
-time_words = [sentence.split(' ') for sentence in times.values()]
+time_words = [sentence.split(" ") for sentence in times.values()]
 
 W, H = 22, 22
+
 
 @dataclasses.dataclass
 class Location:
@@ -72,7 +98,7 @@ class Location:
     def banned(self):
         rv = set()
         rv.add((self.xs, self.ys))
-        
+
         # ban words immediately below a horizontal word (or next to vertical)
         for idx in range(self.length):
             if self.xd > 0:
@@ -92,29 +118,30 @@ class Location:
 
         return rv
 
+
 class Grid(list):
     def __init__(self, backup=None):
-        super().__init__(backup if backup else [[' '] * W for y in range(H)])
+        super().__init__(backup if backup else [[" "] * W for y in range(H)])
 
     def flip(self):
         self[:] = [list(reversed(line)) for line in reversed(self)]
 
     def used(self):
-        return sum(sum(0 if letter == ' ' else 1 for  letter in line) for line in self)
+        return sum(sum(0 if letter == " " else 1 for letter in line) for line in self)
 
     def search(self, x, y, word, banned, max_distance=None, insert_if_empty=False):
-        '''
+        """
         Search for a word in the grid.
         Returns end position (x, y) and (xd, yd) direction vector
-        '''
+        """
         start_x, start_y = x, y
         priority_q = []
         seen = set()
 
         def push(x, y):
-            '''
+            """
             Push item to heaped priority queue, with priority as distance from origin
-            '''
+            """
             if x >= W or y >= H:
                 return
             if (x, y) in seen:
@@ -123,7 +150,7 @@ class Grid(list):
                 push(x + 1, y)
                 push(x, y + 1)
                 return
-        
+
             distance_start = math.sqrt((x - start_x) ** 2 + (y - start_y) ** 2)
             distance_origin = math.sqrt((x - 0) ** 2 + (y - 0) ** 2)
 
@@ -133,12 +160,13 @@ class Grid(list):
             seen.add((x, y))
             heapq.heappush(priority_q, (distance_origin, x, y))
 
-
         def check(x, y, xd, yd):
-            'Check if the position is valid for the word'
+            "Check if the position is valid for the word"
             for idx, letter in enumerate(word):
                 grid_letter = self[y + idx * yd][x + idx * xd]
-                if grid_letter != letter and not (insert_if_empty and grid_letter == ' '):
+                if grid_letter != letter and not (
+                    insert_if_empty and grid_letter == " "
+                ):
                     return False
             return True
 
@@ -146,7 +174,9 @@ class Grid(list):
         while priority_q:
             (distance, x, y) = heapq.heappop(priority_q)
 
-            directions = ([(+1, 0)] if x + len(word) <= W else []) + ([(0, +1)] if y + len(word) <= H else []) 
+            directions = ([(+1, 0)] if x + len(word) <= W else []) + (
+                [(0, +1)] if y + len(word) <= H else []
+            )
             random.shuffle(directions)
 
             for (xd, yd) in directions:
@@ -154,17 +184,19 @@ class Grid(list):
                     if insert_if_empty:
                         for idx, letter in enumerate(word):
                             self[y + idx * yd][x + idx * xd] = letter
-                    return Location(x, y, x + (len(word) - 1) * xd, y + (len(word) - 1) * yd, xd, yd)
+                    return Location(
+                        x, y, x + (len(word) - 1) * xd, y + (len(word) - 1) * yd, xd, yd
+                    )
 
             push(x + 1, y)
             push(x, y + 1)
 
         return None
-    
+
     def insert(self, words):
-        '''
+        """
         Try insert a set of words, return their locations
-        '''
+        """
         x, y = 0, 0
         failed = False
         locations = []
@@ -188,27 +220,42 @@ class Grid(list):
 
             # print(' '.join(words), '--', ' '.join(f'{l.xs}:{l.ys}' for l in locations), list(failed[idx]))
             search_configs = [
-                (3, False), (1, True),
-                (5, False), (3, True),
-                (7, False), (5, True),
-                (10, False), (7, True),
-                (15, False), (10, True),
-                (20, False), (15, True),
-                (None, False), (None, True),
+                (3, False),
+                (1, True),
+                (5, False),
+                (3, True),
+                (7, False),
+                (5, True),
+                (10, False),
+                (7, True),
+                (15, False),
+                (10, True),
+                (20, False),
+                (15, True),
+                (None, False),
+                (None, True),
             ]
             if idx == len(words) - 1:
-                search_configs = [(None, False), (None, True)] 
-
+                search_configs = [(None, False), (None, True)]
 
             for (max_distance, insert_if_empty) in search_configs:
-                found = self.search(x, y, word, banned, max_distance=max_distance, insert_if_empty=insert_if_empty)
+                found = self.search(
+                    x,
+                    y,
+                    word,
+                    banned,
+                    max_distance=max_distance,
+                    insert_if_empty=insert_if_empty,
+                )
 
                 if found:
                     break
 
             # if we still haven't found anything, try on a new line
             if not found:
-                found = self.search(0, y + len(words[idx - 1]), word, banned, insert_if_empty=True)
+                found = self.search(
+                    0, y + len(words[idx - 1]), word, banned, insert_if_empty=True
+                )
 
             if not found:
                 try:
@@ -219,14 +266,14 @@ class Grid(list):
                 try:
                     prev = locations[-1]
                 except IndexError:
-                    prev = Location(0,0,0,0,0,0)
+                    prev = Location(0, 0, 0, 0, 0, 0)
                 # print(x, y, words, word)
                 # print(locations)
                 before = self.used()
                 # print(self.render())
                 self[:] = backups.pop()
                 if self.used() != before:
-                    print(before, 'AFTER', self.used())
+                    print(before, "AFTER", self.used())
                 # print(self.render())
                 x = prev.xs
                 y = prev.ys
@@ -246,29 +293,29 @@ class Grid(list):
 
             banned.update(found.banned)
 
-
         return locations
 
     def render(self, highlight=set()):
-        output = ''
+        output = ""
+
         def render(x, y, letter):
             if (x, y) in highlight:
                 # apply a green highlight
-                letter = '\033[1;32m' + letter + '\033[0m'
+                letter = "\033[1;32m" + letter + "\033[0m"
             return letter
-            
+
         for y, line in enumerate(self):
-            output += (''.join(
-                render(x, y, letter)
-                for x, letter in enumerate(line)
-            ) + '\n')
+            output += (
+                "".join(render(x, y, letter) for x, letter in enumerate(line)) + "\n"
+            )
         return output
 
     def fill(self):
         for x in range(W):
             for y in range(H):
-                if self[x][y] == ' ':
+                if self[x][y] == " ":
                     self[x][y] = random.choice(string.ascii_lowercase)
+
 
 inserted = 0
 while inserted < len(times):
@@ -291,38 +338,43 @@ while inserted < len(times):
     # print(grid.render())
 
     # Flip back and insert full phrases
-    print('Inserting full phrases')
+    print("Inserting full phrases")
     grid.flip()
     random_time = None
     inserted = 0
     for time, sentence in times.items():
-        locations[time] = grid.insert(sentence.split(' '))
+        locations[time] = grid.insert(sentence.split(" "))
         if not locations[time]:
-            print(f'Failed after {inserted} of {len(times)} times [{W}x{H}]')
+            print(f"Failed after {inserted} of {len(times)} times [{W}x{H}]")
             break
         inserted += 1
 
-print(f'Inserted {inserted} of {len(times)} times [{W}x{H}]')
+print(f"Inserted {inserted} of {len(times)} times [{W}x{H}]")
 
 # grid.fill()
 
 time_highlight = {}
 for time, letters in locations.items():
     highlight = set()
-    for location in (letters or []):
+    for location in letters or []:
         x, y, xe, ye, xd, yd = dataclasses.astuple(location)
         while x <= xe and y <= ye:
             highlight.add((x, y))
             (x, y) = x + xd, y + yd
     time_highlight[time] = highlight
 
-with open('output.json', 'w') as f:
-    json.dump({
-        ('%02d:%02d' % time): list(highlight) for time, highlight in time_highlight.items()
-    }, f, sort_keys=True) 
-    f.write('\n')
+with open("output.json", "w") as f:
+    json.dump(
+        {
+            ("%02d:%02d" % time): list(highlight)
+            for time, highlight in time_highlight.items()
+        },
+        f,
+        sort_keys=True,
+    )
+    f.write("\n")
 
 for time, highlight in time_highlight.items():
-    sys.stdout.write('\033[2J' + '%02d:%02d' % time + '\n' + grid.render(highlight))
+    sys.stdout.write("\033[2J" + "%02d:%02d" % time + "\n" + grid.render(highlight))
     sys.stdout.flush()
     sleep(0.1)
